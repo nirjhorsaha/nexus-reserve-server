caae02987f64132e2e102aed04ded525ff79c75e
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-06-12 16:08:03 +0600

    server [modified]

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
-// import notFound from './app/middlewares/notFound';
-// import router from './app/routes';
+import router from './app/routes';
 import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 
 const app = express();
 
@@ -9,14 +10,16 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-// app.use('/api/v1', router);
+app.use('/api/v1', router);
 
 // root route
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to our Co-Working Space Booking System!');
 });
 
+app.use(globalErrorHandler);
+
 //Not Found
-// app.use(notFound);
+app.use(notFound);
 
 export default app;
