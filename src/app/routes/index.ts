bf40a12b1908dd551025bda152657f1707790182
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-06-13 11:47:07 +0600

    centralized router setup for modular route handling

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,12 +1,26 @@
 import { Router } from 'express';
 import { AuthRoutes } from '../modules/Auth/auth.route';
-import { UserRoutes } from '../modules/User/user.route';
+import { RoomsRoutes } from '../modules/Room/room.route';
+import { SlotRoutes } from '../modules/Slot/slot.route';
+import { BookingRoutes } from '../modules/Booking/booking.route';
 const router = Router();
 
 const moduleRoutes = [
   {
     path: '/auth',
     route: AuthRoutes,
+  },
+  {
+    path: '/rooms',
+    route: RoomsRoutes,
+  },
+  {
+    path: '/slots',
+    route: SlotRoutes,
+  },
+  {
+    path: '/bookings ',
+    route: BookingRoutes,
   },
 ];
 
