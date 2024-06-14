315bc3154e068540f9262e416ba047521b1b257f
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-06-14 15:54:01 +0600

    Implement createRoom function to handle room creation requests

--- src/app/modules/Room/room.controller.ts
+++ src/app/modules/Room/room.controller.ts
@@ -1,47 +1,43 @@
 import { Request, Response } from 'express';
 import { RoomService } from './room.service';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import httpStatus from 'http-status';
 
-const createRoom = async (req: Request, res: Response) => {
-  try {
-    const { name, roomNo, floorNo, capacity, pricePerSlot, amenities } = req.body;
+const createRoom = catchAsync(async (req: Request, res: Response) => {
+  const { name, roomNo, floorNo, capacity, pricePerSlot, amenities } = req.body;
 
-    // Validation for roomNo based on floorNo
-    const roomNoPattern = (floorNo * 100) + 100; // Base starting room number for the floor
-    const roomNoStart = roomNoPattern + 1; // Start of the valid room number range for the floor
-    const roomNoEnd = roomNoPattern + 100; // End of the valid room number range for the floor
+  // Validation for roomNo based on floorNo
+  const roomNoPattern = floorNo * 100 + 100; // Base starting room number for the floor
+  const roomNoStart = roomNoPattern + 1; // Start of the valid room number range for the floor
+  const roomNoEnd = roomNoPattern + 100; // End of the valid room number range for the floor
 
-    if (roomNo < roomNoStart || roomNo > roomNoEnd) {
-      return res.status(400).json({
-        success: false,
-        statusCode: 400,
-        message: `Invalid roomNo. For floorNo ${floorNo}, roomNo should be between ${roomNoStart} and ${roomNoEnd}.`,
-      });
-    }
-
-    const newRoom = await RoomService.createRoom({
-      name,
-      roomNo,
-      floorNo,
-      capacity,
-      pricePerSlot,
-      amenities,
-      isDeleted: false,
-    });
-
-    res.status(200).json({
-      success: true,
-      statusCode: 200,
-      message: 'Room added successfully',
-      data: newRoom,
-    });
-  } catch (error:any) {
-    res.status(500).json({
+  if (roomNo < roomNoStart || roomNo > roomNoEnd) {
+    return sendResponse(res, {
       success: false,
-      statusCode: 500,
-      message: error.message,
+      statusCode: httpStatus.BAD_REQUEST,
+      message: `Invalid roomNo. For floorNo ${floorNo}, roomNo should be between ${roomNoStart} and ${roomNoEnd}.`,
+      data: null,
     });
   }
-};
+
+  const newRoom = await RoomService.createRoom({
+    name,
+    roomNo,
+    floorNo,
+    capacity,
+    pricePerSlot,
+    amenities,
+    isDeleted: false,
+  });
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'Room added successfully',
+    data: newRoom,
+  });
+});
 export const RoomController = {
-     createRoom
- }
+  createRoom,
+};
