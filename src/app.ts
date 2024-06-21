cd5cd6a1d8138390f91ec8bbf270c2ddd6117a53
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-06-21 11:39:13 +0600

    [Implement] user booking route

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import router from './app/routes';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import { BookingController } from './app/modules/Booking/booking.controller';
+import { authenticateUser } from './app/middlewares/auth';
 
 const app = express();
 
@@ -11,6 +13,11 @@ app.use(cors());
 
 // application routes
 app.use('/api', router);
+app.use(
+  '/api/my-bookings',
+  authenticateUser,
+  BookingController.getUserBookings,
+);
 
 // root route
 app.get('/', (req: Request, res: Response) => {
