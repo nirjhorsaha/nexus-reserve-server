b218ecc13a2c0e6c877aefe8059ab2ed2de2d531
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-06-21 17:32:22 +0600

    fix lint

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-import validateRequest from '../../middlewares/validateRequest';
 import { userController } from '../User/user.controller';
-import { UserValidation } from '../User/user.validation';
 
 const router = express.Router();
 
