f5da812497ad15aece6d788704dcf2e69232c483
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-06-21 11:40:14 +0600

    [Modified] authentication middleware

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response, NextFunction } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
