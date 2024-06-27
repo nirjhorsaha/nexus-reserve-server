f465e27edd07c30b9cbe0b6953494be8a4384397
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-06-27 08:35:35 +0600

    🎨 Use not data found utility in get all user

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -2,6 +2,7 @@ import httpStatus from 'http-status';
 import sendResponse from '../../utils/sendResponse';
 import { UserService } from './user.service';
 import catchAsync from '../../utils/catchAsync';
+import noDataFound from '../../middlewares/noDataFound';
 
 const userSignUp = catchAsync(async (req, res) => {
   const user = await UserService.createUser(req.body);
@@ -18,20 +19,14 @@ const getAllUsers = catchAsync(async (req, res) => {
   const users = await UserService.getAllUsers();
 
   if (users.length === 0) {
-    sendResponse(res, {
-      statusCode: httpStatus.NOT_FOUND,
-      success: false,
-      message: 'No Data Found',
-      data: [],
-    });
-  } else {
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Users retrieved successfully',
-      data: users,
-    });
+    return noDataFound(res);
   }
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Users retrieved successfully',
+    data: users,
+  });
 });
 
 export const userController = {
