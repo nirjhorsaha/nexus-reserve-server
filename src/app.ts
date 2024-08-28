43b5bd0827a049173a0eea93dc2ce2d6343e6cc6
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-08-28 17:03:21 +0600

    🔧 Update cors and cookies

--- src/app.ts
+++ src/app.ts
@@ -3,26 +3,27 @@ import router from './app/routes';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
-import { BookingController } from './app/modules/Booking/booking.controller';
-import { authenticateUser } from './app/middlewares/auth';
+import cookieParser from 'cookie-parser';
+
 
 const app = express();
-// app.use(
-//   cors({
-//     origin: ['http://localhost:5173',''],
-//     credentials: true,
-//   }),
-// );
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  }),
+);
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+// app.use(cors());
 
 // Application routes
 app.use('/api', router);
-app.use(
-  '/api/my-bookings',
-  authenticateUser,
-  BookingController.getUserBookings,
-);
+// app.use(
+//   '/api/my-bookings',
+//   authenticateUser,
+//   BookingController.getUserBookings,
+// );
 
 // Root route
 app.get('/', (req: Request, res: Response) => {
--- src/app/utils/jwt.ts
+++ src/app/utils/jwt.ts
@@ -1,10 +1,14 @@
-import jwt from 'jsonwebtoken';
-import config from '../config';
+// import jwt from 'jsonwebtoken';
+// import config from '../config';
 
-const generateToken = (email: string, role: string) => {
-  return jwt.sign({ email, role }, config.jwt_access_secret as string, {
-    expiresIn: config.jwt_access_expires_in,
-  });
-};
+// export const generateToken = (email: string, role: string) => {
+//   return jwt.sign({ email, role }, config.jwt_access_secret as string, {
+//     expiresIn: config.jwt_access_expires_in,
+//   });
+// };
 
-export default generateToken;
+// export const generateRefreshToken = (email: string, role: string) => {
+//   return jwt.sign({ email, role }, config.jwt_refresh_secret as string, {
+//     expiresIn: config.jwt_refresh_expires_in,
+//   });
+// };
